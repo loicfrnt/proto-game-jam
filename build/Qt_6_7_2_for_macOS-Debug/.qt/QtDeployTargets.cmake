856e9b707ed59a405ad28f179bbf518f03117726
Loic Fournerat <lfournerat@corp.free.fr>
2024-08-24 14:04:52 +0200

    init qt/qml project

---
+++ build/Qt_6_7_2_for_macOS-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_appproto-game-jam_FILE /Users/marcassin/taff/learning_QT/proto-game-jam/build/Qt_6_7_2_for_macOS-Debug/appproto-game-jam.app/Contents/MacOS/appproto-game-jam)
+set(__QT_DEPLOY_TARGET_appproto-game-jam_TYPE EXECUTABLE)
---
+++ build/Qt_6_7_2_for_macOS-Debug/appproto-game-jam_qmltyperegistrations.cpp
@@ -0,0 +1,22 @@
+/****************************************************************************
+** Generated QML type registration code
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <QtQml/qqml.h>
+#include <QtQml/qqmlmoduleregistration.h>
+
+
+
+#if !defined(QT_STATIC)
+#define Q_QMLTYPE_EXPORT Q_DECL_EXPORT
+#else
+#define Q_QMLTYPE_EXPORT
+#endif
+Q_QMLTYPE_EXPORT void qml_register_types_proto_game_jam()
+{
+    qmlRegisterModule("proto-game-jam", 1, 0);
+}
+
+static const QQmlModuleRegistration protogamejamRegistration("proto-game-jam", qml_register_types_proto_game_jam);
